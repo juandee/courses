fbcab27374e94f4400f1cb6518a9f323634319c9
Juan Desiderio <juan_nac05@hotmail.com>
2017-12-22 01:24:55 -0300

    manejo de sesiones - devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :courses do
-  	resources :exams do
-  		resources :grades
-  	end
-  	resources :pupils
-  end
+  	devise_for :users
+	resources :courses do
+  		resources :exams do
+  			resources :grades
+  		end
+  		resources :pupils
+	end
+	root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
